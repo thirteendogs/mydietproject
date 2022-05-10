1d87b15ccb45b32902a865b28de268ae97de7fa0
Daniel <daniel.nova.girbau@gmail.com>
2022-05-10 14:40:18 +0200

    Add footer test

---
+++ src/components/tests/Footer.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+
+import Footer from '../globals/Footer'
+
+describe('Footer component', () => {
+  beforeEach(() => {
+    render(<Footer />)
+  })
+
+  test('Footer to have correct text', () => {
+    const footerText = screen.getAllByText('Crafted by Thirteendogs 🙃')
+    expect(footerText).toBeDefined()
+  })
+})
