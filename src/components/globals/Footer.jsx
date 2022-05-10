2b1a644f4abdc86d17c0e2231d964b3ea250a43e
Daniel <daniel.nova.girbau@gmail.com>
2022-05-10 02:24:44 +0200

    Add footer

---
+++ src/components/globals/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+      <footer className='footer'>
+         <p className='footer__text'>Crafted by Thirteendogs 🙃</p>
+      </footer>
+  )
+}
+
+export default Footer
